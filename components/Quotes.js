1f2fdee49b09c6a8bf3289cb71b67dad0a66cf66
AvigailM <40786714+AvigailM@users.noreply.github.com>
2018-10-20 19:28:12 +0300

    new quote API
    https://safe-garden-90262.herokuapp.com/api/quote

--- components/Quotes.js
+++ components/Quotes.js
@@ -2,38 +2,51 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
-const API = "https://talaikis.com/api/quotes/random";
+const API = "https://safe-garden-90262.herokuapp.com/api/quote";
+//https://quotes.stormconsultancy.co.uk/random.json";
+//"https://talaikis.com/api/quotes/random";
 
 class Quotes extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      quote: "",
-      author: "",
-      isLoading: false,
-      error: null
-    };
-  }
+  state = {
+    quote: "",
+    author: "",
+    isLoading: false,
+    error: null
+  };
+
+  signal = axios.CancelToken.source();
 
   componentDidMount() {
-    this.setState({ isLoading: true });
-
-    axios
-      .get(API)
-      .then(result =>
-        this.setState({
-          quote: result.data.quote,
-          author: result.data.author,
-          isLoading: false
-        })
-      )
-      .catch(error =>
-        this.setState({
-          error,
-          isLoading: true
-        })
-      );
+    this.onLoadQuote();
   }
+
+  componentWillUnmount() {
+    this.signal.cancel("Api is being canceled");
+  }
+
+  onLoadQuote = async () => {
+    try {
+      this.setState({ isLoading: true });
+      const response = await axios.get(API, {
+        cancelToken: this.signal.token
+      });
+      this.setState({
+        quote: response.data.quoteText,
+        author: response.data.quoteAuthor,
+        isLoading: false
+      });
+    } catch (err) {
+      if (axios.isCancel(err)) {
+        this.setState({
+          error: err,
+          isLoading: true
+        });
+        console.log("Error: ", err.message); // => prints: Api is being canceled
+      } else {
+        this.setState({ isLoading: false });
+      }
+    }
+  };
 
   render() {
     const { quote, author, isLoading, error } = this.state;
